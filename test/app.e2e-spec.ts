7d14fa853826e4949cc181d2781420c4b23fe463
ExcitedHumvee <35200248+ExcitedHumvee@users.noreply.github.com>
2024-02-14 00:38:00 +0300

    Added e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -5,6 +5,7 @@ import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let short='';
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -22,6 +23,12 @@ describe('AppController (e2e)', () => {
       .expect('Hello World!');
   });
 
+  it('/urlMaps/deleteAll (DELETE)', async () => {
+    await request(app.getHttpServer())
+      .delete('/urlMaps/deleteAll')
+      .expect(200);
+  });
+
   it('/urlMaps (GET)', async () => {
     const response = await request(app.getHttpServer())
       .get('/urlMaps')
@@ -30,9 +37,27 @@ describe('AppController (e2e)', () => {
     expect(response.body).toEqual(expect.any(Array));
   });
 
+  it('/shortenUrl (POST)', async () => {
+    const dto = { longUrl: 'https://example.com', aliasURL: '', requestLimit: 10 };
+    const response = await request(app.getHttpServer())
+      .post('/shortenUrl')
+      .send(dto)
+      .expect(201);
+    short=response.body.shortUrl;
+    expect(response.body).toHaveProperty('shortUrl');
+  });
+
+  it('/:shortUrl (GET)', async () => {
+    // Assuming you have a short URL in your database for testing
+    const shortUrl = short;
+    await request(app.getHttpServer())
+      .get(`/${shortUrl}`)
+      .expect(302); // Expecting a redirect
+  });
+
   it('/:shortUrl/statistics (GET)', async () => {
     // Assuming you have a short URL in your database for testing
-    const shortUrl = 'example';
+    const shortUrl = short;
     const response = await request(app.getHttpServer())
       .get(`/${shortUrl}/statistics`)
       .expect(200);
@@ -42,12 +67,26 @@ describe('AppController (e2e)', () => {
     // Add more assertions as needed
   });
 
-  it('/:shortUrl (GET)', async () => {
-    // Assuming you have a short URL in your database for testing
-    const shortUrl = 'example';
-    await request(app.getHttpServer())
-      .get(`/${shortUrl}`)
-      .expect(302); // Expecting a redirect
+  it('/urlMap (PUT)', async () => {
+    // Assuming you have an existing short URL in your database for testing
+    const updateDto = { shortURL: short, requestLimit: 20 };
+    const response = await request(app.getHttpServer())
+      .put('/urlMap')
+      .send(updateDto)
+      .expect(200);
+
+    expect(response.body);
+  });
+
+  it('/urlMap (DELETE)', async () => {
+    // Assuming you have an existing short URL in your database for testing
+    const deleteDto = { shortURL: short };
+    const response = await request(app.getHttpServer())
+      .delete('/urlMap')
+      .send(deleteDto)
+      .expect(200);
+
+    expect(response.body);
   });
 
   afterAll(async () => {
